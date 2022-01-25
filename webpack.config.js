2400957297d036de20dc18e4c53cd0e1c42c9424
trujilml <mickeytrj@gmail.com>
2022-01-24 19:53:44 -0600

    ran webpack and fixed bootstrap popper and js related errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = { entry: './assets/js/script.js', 
                    output: {
                        path: path.resolve(__dirname, 'dist'),
                        filename: 'main.bundle.js'
                    },
-                   mode: 'development'
+                   mode: 'development',
+                   plugins: [
+                     new webpack.ProvidePlugin({
+                       $: "jquery",
+                       jQuery: "jquery"
+                     }),
+                   ],
                  };
