6fcb4cddb637c7ba93e42def85eba58d7eb47818
trujilml <mickeytrj@gmail.com>
2022-01-25 19:02:21 -0600

    added multiple entry points to the webpack's configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 
 module.exports = { entry: './assets/js/script.js', 
                    output: {
-                       path: path.resolve(__dirname, 'dist'),
-                       filename: 'main.bundle.js'
+                       path: __dirname + '/dist',
+                       filename: '[name].bundle.js'
                    },
                    mode: 'development',
                    plugins: [
@@ -17,4 +17,10 @@ module.exports = { entry: './assets/js/script.js',
                        analyzerMode: "static", //report outputs to a html file in the dist folder
                      })
                    ],
+                   entry: {
+                     app: "./assets/js/script.js",
+                     events: "./assets/js/events.js",
+                     schedule: "./assets/js/schedule.js",
+                     tickets: "./assets/js/tickets.js"
+                   }
                  };
